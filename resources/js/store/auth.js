f6906acd483f465a9c2921bdbfac96d5a1ebdb84
Ryusei <sidapple@gmail.com>
2022-08-21 16:53:09 +0900

    バリデーションエラー表示処理を追加

--- resources/js/store/auth.js
+++ resources/js/store/auth.js
@@ -1,8 +1,10 @@
-import { OK } from '../util'
+import { OK, CREATED, UNPROCESSABLE_ENTITY } from '../util'
 
 const state = {
   user: null,
-  apiStatus: null
+  apiStatus: null,
+  loginErrorMessages: null,
+  registerErrorMessages: null
 }
 
 const getters = {
@@ -16,6 +18,12 @@ const mutations = {
   },
   setApiStatus (state, status) {
     state.apiStatus = status
+  },
+  setLoginErrorMessages (state, messages) {
+    state.loginErrorMessages = messages
+  },
+  setRegisterErrorMessages (state, messages) {
+    state.registerErrorMessages = messages
   }
 }
 
@@ -23,6 +31,19 @@ const actions = {
   async register (context, data) {
     const response = await axios.post('/api/register', data)
     context.commit('setUser', response.data)
+
+    if (response.status === CREATED) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', response.data)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      context.commit('setRegisterErrorMessages', response.data.errors)
+    } else {
+      context.commit('error/setCode', response.status, { root: true })
+    }
   },
   async login (context, data) {
     context.commit('setApiStatus', null)
@@ -38,18 +59,42 @@ const actions = {
     }
     // 失敗したらfalse
     context.commit('setApiStatus', false)
-    // 通信に失敗した場合に error モジュールの setCode ミューテーションを commit する
-    // あるストアモジュールから別のモジュールのミューテーションを commit する場合は第三引数に { root: true } を追加
-    context.commit('error/setCode', response.status, { root: true })
+
+    if (response.status === UNPROCESSABLE_ENTITY) {
+      // バリデーションに引っ掛かったらエラーをセット
+      context.commit('setLoginErrorMessages', response.data.errors)
+    } else {
+      // 通信に失敗した場合に error モジュールの setCode ミューテーションを commit する
+      // あるストアモジュールから別のモジュールのミューテーションを commit する場合は第三引数に { root: true } を追加
+      context.commit('error/setCode', response.status, { root: true })
+    }
   },
   async logout (context) {
     const response = await axios.post('/api/logout')
     context.commit('setUser', null)
+
+    if (response.status === OK) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', null)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    context.commit('error/setCode', response.status, { root: true })
   },
   async currentUser (context) {
     const response = await axios.get('/api/user')
     const user = response.data || null
     context.commit('setUser', user)
+
+    if (response.status === OK) {
+      context.commit('setApiStatus', true)
+      context.commit('setUser', user)
+      return false
+    }
+
+    context.commit('setApiStatus', false)
+    context.commit('error/setCode', response.status, { root: true })
   }
 }
 
