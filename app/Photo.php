774176a5add008cf823b4f2183f4c203ad22a85a
Ryusei <sidapple@gmail.com>
2022-08-22 21:01:23 +0900

    Model追加

---
+++ app/Photo.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Arr;
+
+class Photo extends Model
+{
+    /** プライマリキーの値を初期設定（int）から変更したい場合は $keyType を上書きする */
+    protected $keyType = 'string';
+
+    /** IDの桁数 */
+    const ID_LENGTH = 12;
+
+    public function __construct(array $attributes = [])
+    {
+        /** 親クラスのコンストラクタを実行することで、Modelの初期化に必要な処理が実行される */
+        parent::__construct($attributes);
+
+        if (!Arr::get($this->attributes, 'id')) {
+            $this->setId();
+        }
+    }
+
+    /**
+     * ランダムなID値をid属性に代入する
+     */
+    private function setId()
+    {
+        $this->attributes['id'] = $this->getRandomId();
+    }
+
+    /**
+     * ランダムなID値を生成する
+     * @return string
+     */
+    private function getRandomId()
+    {
+        $characters = array_merge(
+            range(0, 9), range('a', 'z'),
+            range('A', 'Z'), ['-', '_']
+        );
+
+        $length = count($characters);
+
+        $id = "";
+
+        for ($i = 0; $i < self::ID_LENGTH; $i++) {
+            $id .= $characters[random_int(0, $length - 1)];
+        }
+
+        return $id;
+    }
+}
